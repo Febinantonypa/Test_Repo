f010ded83af497b0f526772956a30dbb7c46af69
Febin Antony <febin@jobinandjismi.com>
2022-04-29 18:56:38 +0530

    second version

--- Test_Project/src/FileCabinet/SuiteScripts/Developer Certification/sdr_ue_clientside_validations.js
+++ Test_Project/src/FileCabinet/SuiteScripts/Developer Certification/sdr_ue_clientside_validations.js
@@ -4,7 +4,7 @@
  * confidential and proprietary information of NetSuite, Inc. ("Confidential
  * Information"). You shall not disclose such Confidential Information and shall
  * use it only in accordance with the terms of the license agreement you entered
- * into with NetSuite.
+ * into with NetSuite. shfwisfhishfigfsshf
  */
 
 /**
